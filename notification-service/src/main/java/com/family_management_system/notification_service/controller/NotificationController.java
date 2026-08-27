d4b3d6edcfd2d888ebcddb51a35baa43a3aca167
nirmal <nirmalsivadas2017@gmail.com>
2026-08-27 18:46:05 +0530

    frontend and backend

--- notification-service/src/main/java/com/family_management_system/notification_service/controller/NotificationController.java
+++ notification-service/src/main/java/com/family_management_system/notification_service/controller/NotificationController.java
@@ -1,15 +1,16 @@
 package com.family_management_system.notification_service.controller;
 
+import com.family_management_system.notification_service.dto.ApiResponse;
 import com.family_management_system.notification_service.dto.NotificationResponse;
 import com.family_management_system.notification_service.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/notifications")
@@ -19,12 +20,43 @@ public class NotificationController {
     private NotificationService notificationService;
 
     @GetMapping
-    public ResponseEntity<ApiResponse<NotificationResponse>> notifications(){
-        ApiResponse<NotificationResponse> response = new ApiResponse(
+    public ResponseEntity<ApiResponse<Page<NotificationResponse>>> notifications(
+            @RequestParam Long userId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ){
+        ApiResponse<Page<NotificationResponse>> response = new ApiResponse(
                 "notifications fetched",
                 HttpStatus.OK,
                 LocalDateTime.now(),
-                notificationService.getAllNotifications()
+                notificationService.getAllNotifications(userId,page,size)
+        );
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/top-5")
+    public ResponseEntity<ApiResponse<List<NotificationResponse>>> getTop5notifications(
+            @RequestParam Long userId
+    ){
+        ApiResponse<List<NotificationResponse>> response = new ApiResponse(
+                "Top 5 notifications fetched",
+                HttpStatus.OK,
+                LocalDateTime.now(),
+                notificationService.getTop5Notifications(userId)
+        );
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/mark-as-read/{notificationId}")
+    public ResponseEntity<ApiResponse<String>> markAsRead(
+            @RequestParam Long userId,
+            @PathVariable Long notificationId
+    ){
+        ApiResponse<String> response = new ApiResponse(
+                "marked notification as read",
+                HttpStatus.OK,
+                LocalDateTime.now(),
+                notificationService.markAsRead(userId,notificationId)
         );
         return ResponseEntity.ok(response);
     }
--- notification-service/src/main/java/com/family_management_system/notification_service/service/impl/NotificationServiceImpl.java
+++ notification-service/src/main/java/com/family_management_system/notification_service/service/impl/NotificationServiceImpl.java
@@ -1,17 +1,62 @@
 package com.family_management_system.notification_service.service.impl;
 
 import com.family_management_system.notification_service.dto.NotificationResponse;
+import com.family_management_system.notification_service.entity.Notification;
 import com.family_management_system.notification_service.repository.NotificationRepository;
 import com.family_management_system.notification_service.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class NotificationServiceImpl implements NotificationService {
     @Autowired
     private NotificationRepository notificationRepository;
     @Override
-    public NotificationResponse getAllNotifications() {
-        return notificationRepository.;
+    public Page<NotificationResponse> getAllNotifications(Long userId,int page,int size) {
+        Pageable pageable = PageRequest.of(page,size, Sort.by("timeStamp").descending());
+        Page<Notification> notifications = notificationRepository.findByUserId(userId,pageable);
+        return notifications.map(n->new NotificationResponse(
+                n.getTitle(),
+                n.getMessage(),
+                n.isMarkAsRead(),
+                n.getTimeStamp()
+        ));
+    }
+
+    @Override
+    public List<NotificationResponse> getTop5Notifications(Long userId) {
+        List<Notification> notifications = notificationRepository.findTop5ByUserIdOrderByTimeStampDesc(userId);
+        return notifications.stream().map(n->new NotificationResponse(
+                n.getTitle(),
+                n.getMessage(),
+                n.isMarkAsRead(),
+                n.getTimeStamp()
+        )).toList();
+    }
+
+    @Override
+    public String markAsRead(Long userId,Long notificationId) {
+        Notification notification = notificationRepository.findById(notificationId)
+                .orElseThrow(()->new RuntimeException("Notification not found"));
+        notification.setMarkAsRead(true);
+        notificationRepository.save(notification);
+        return "Notification marked as read";
+    }
+
+    @Override
+    public NotificationResponse generateNotification(Long userId){
+        return new NotificationResponse(
+                "Notification",
+                "Notification generated",
+                false,
+                LocalDateTime.now()
+        );
     }
 }
